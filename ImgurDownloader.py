dea3c302dc6d624a1d5dfb46b306e8544b7add8b
Imperial_Squid <benjaeger99@gmail.com>
2018-11-04 02:26:20 +0000

    Added functionality to init Removed unneeded method Started writing download method

--- ImgurDownloader.py
+++ ImgurDownloader.py
@@ -1,9 +1,23 @@
 from bs4 import BeautifulSoup as BS
 import re
+import requests as req
+from ImgurExceptions import ImgurURLException
+
 
 class ImgurDownloader:
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, url, albumName=None, albumKeyInName = False, imageKeyInName = False, ):
+        if self.testURL(self, url):
+            self.url = url
+            self.albumName = albumName
+            self.albumKeyInName = albumKeyInName
+            self.imageKeyInName = imageKeyInName
+        else:
+            raise ImgurURLException("URL not recognised")
 
-    def testUrl(self, url):
+    def testURL(self, url):
         return re.match("(https?)\:\/\/(www\.)?(?:m\.)?imgur\.com/(a|gallery)/([a-zA-Z0-9]+)(#[0-9]+)?", url)
+
+    def download(self):
+        albumKey = re.match("(https?)\:\/\/(www\.)?(?:m\.)?imgur\.com/(a|gallery)/([a-zA-Z0-9]+)(#[0-9]+)?", self.url).group(4)
+        fullURL = "http://imgur.com/a/"+albumKey+"/layout/blog"
+        print(fullURL)
